cb85d52547d802d96b9b78c8a2e436d1e1ba8696
Anagaraghavan <anagaraghavan04@gmail.com>
2024-08-19 09:38:22 +0530

    Update frequency.java

--- Asiet/S3/OOPS/lab/Cycle2/frequency.java
+++ Asiet/S3/OOPS/lab/Cycle2/frequency.java
@@ -1,1 +1,24 @@
-
+//Frequency of a string
+//Anaga Raghavan
+//Roll No: 8
+import java.util.Scanner;
+class Frequency
+{
+ public static void main(String args[])
+{
+	System.out.println("enter a string:");
+	Scanner sc=new Scanner(System.in) ;
+	String s=sc.nextLine();
+	char ch=sc.next().charAt(0);
+	int frequency=0;
+	for(int i=0;i<s.length();i++)
+	{
+	if(ch==s.charAt(i))
+	{
+	frequency++;
+	}
+	}
+	System.out.println("frequency of "+ch+" is "+frequency);
+	
+}
+}
