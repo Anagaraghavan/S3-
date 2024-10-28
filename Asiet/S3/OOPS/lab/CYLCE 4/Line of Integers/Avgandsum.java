51fa4900ad49d24aeeee51b3aff67350d8572de1
Anagaraghavan <anagaraghavan04@gmail.com>
2024-10-28 10:04:13 +0530

    Update Avgandsum.java

--- Asiet/S3/OOPS/lab/CYLCE 4/Line of Integers/Avgandsum.java
+++ Asiet/S3/OOPS/lab/CYLCE 4/Line of Integers/Avgandsum.java
@@ -1,1 +1,23 @@
+//Anaga Raghavan
+//avg and sum
+import java.util.*;
+class avg
+{
+    public static void main(String[] args) 
+    {
+        Scanner s =new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter Two integers");
+        a=s.nextInt();
+        b=s.nextInt();
+       	int sum=a+b;
+        System.out.println("The sum is"+sum);
+        float avg=sum/2;
+        System.out.println("The average is"+avg);
+        s.close();
+        
 
+    }
+
+    
+}
